b696d44f4b3f4fbc9c80495691c7ee2450184d06
Austin Crane <austincrane@gmail.com>
2019-06-21 10:23:04 -0400

    ;

--- footer.php
+++ footer.php
@@ -17,28 +17,28 @@ $disc = get_field('disclaimer','option');
 			<div class="intro footer-info text-center">
 				<h2 class="section-title"><?php echo get_bloginfo('name'); ?></h2>
 				<?php if ($phone_number) { ?>
-				<div class="phone"><?php echo $phone_number ?></div>	
+				<div class="phone"><?php echo $phone_number; ?></div>	
 				<?php } ?>
 				<?php if ($address_1) { ?>
-				<div class="address add1"><?php echo $address_1 ?></div>	
+				<div class="address add1"><?php echo $address_1; ?></div>	
 				<?php } ?>
 				<?php if ($address_2) { ?>
-				<div class="address add2"><?php echo $address_2 ?></div>	
+				<div class="address add2"><?php echo $address_2; ?></div>	
 				<?php } ?>
 				<?php if ($address_3) { ?>
-				<div class="address add3"><?php echo $address_3 ?></div>	
+				<div class="address add3"><?php echo $address_3; ?></div>	
 				<?php } ?>
 
 				<?php if ($linkedin || $facebook || $twitter) { ?>
 				<div class="social-links">
 					<?php if ($linkedin) { ?>
-					<a href="<?php echo $linkedin ?>" target="_blank"><i class="fab fa-linkedin-in"></i><span class="sr-only">Linkedin</span></a>
+					<a href="<?php echo $linkedin; ?>" target="_blank"><i class="fab fa-linkedin-in"></i><span class="sr-only">Linkedin</span></a>
 					<?php } ?>
 					<?php if ($facebook) { ?>
-					<a href="<?php echo $facebook ?>" target="_blank"><i class="fab fa-facebook-f"></i><span class="sr-only">Facebook</span></a>
+					<a href="<?php echo $facebook; ?>" target="_blank"><i class="fab fa-facebook-f"></i><span class="sr-only">Facebook</span></a>
 					<?php } ?>
 					<?php if ($twitter) { ?>
-					<a href="<?php echo $twitter ?>" target="_blank"><i class="fab fa-twitter"></i><span class="sr-only">Twitter</span></a>
+					<a href="<?php echo $twitter; ?>" target="_blank"><i class="fab fa-twitter"></i><span class="sr-only">Twitter</span></a>
 					<?php } ?>
 				</div>
 				<?php } ?>
