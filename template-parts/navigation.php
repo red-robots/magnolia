fc6499fed599a213f2d33b08890754287557db55
Lisa <annalisa.quiamco@gmail.com>
2019-06-26 21:33:14 -0600

    Added blank target to login button

--- template-parts/navigation.php
+++ template-parts/navigation.php
@@ -7,7 +7,7 @@
 				$login_label = get_field('button_text','option');
 				$nav_options = array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu','container'=>false );
 				if($login_link && $login_label){
-					$nav_options['items_wrap'] = '<ul id="%1$s" class="%2$s">%3$s<li class="custom_menu_link"><a href="'.$login_link.'">'.$login_label.'</a></li></ul>';
+					$nav_options['items_wrap'] = '<ul id="%1$s" class="%2$s">%3$s<li class="custom_menu_link"><a href="'.$login_link.'" target="_blank">'.$login_label.'</a></li></ul>';
 				}
 				wp_nav_menu($nav_options);
 				//wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu','container'=>false ) ); 
